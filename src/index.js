0709e2495dbe12924ab3fe3e959cccb18ffe6c30
caseypollard <cjpollard0407@hotmail.com>
2024-01-06 00:21:49 +1100

    Added axios
    Linked real time temperatures according to city searched

--- src/index.js
+++ src/index.js
@@ -1,3 +1,21 @@
+function displayTemp(response) {
+    let temperatureElement = document.querySelector("#current-temp-value");
+    let cityElement = document.querySelector("#current-city");
+    let currentTemperature = Math.round(response.data.temperature.current);
+    cityElement.innerHTML = response.data.city;
+    temperatureElement.innerHTML = currentTemperature;
+  }
+  
+  function search(event) {
+    event.preventDefault();
+    let searchInputElement = document.querySelector("#search-input");
+    let city = searchInputElement.value;
+    let apiKey = "8c48ta45fbf8of4c9026e9ea057383be";
+    let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=metric`;
+  
+    axios.get(apiUrl).then(displayTemp);
+  }
+
 function dateElement(date) {
     let minute = date.getMinutes();
     let hour = date.getHours();
@@ -23,6 +41,9 @@ function dateElement(date) {
     let formattedDay = days[day];
     return `${formattedDay} ${hour}:${minute}`;
 }
+
+let searchForm = document.querySelector("#search-form");
+searchForm.addEventListener("submit", search);
 
 let currentDateElement = document.querySelector("#current-date");
 let currentDate = new Date();
